a4b75f5e32854970c24dfded8052b184094de181
Den1k22 <komarden22@gmail.com>
2022-06-21 12:41:56 +0300

    init commit

---
+++ LearningSwiftMobile/LearningSwiftMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftMobileApp.swift
+//  LearningSwiftMobile
+//
+//  Created by Deniss Komarskis on 21.6.2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
