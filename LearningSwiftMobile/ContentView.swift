7e1c30417af9a8c17e4afec752ebd7595c8a2ce9
Den1k22 <komarden22@gmail.com>
2022-06-21 17:06:26 +0300

    impr: added counter example

--- LearningSwiftMobile/ContentView.swift
+++ LearningSwiftMobile/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var count: Int = 0
+    @State var randomNumberResult: Int = 0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Current count \(count)")
+                .padding(10)
+                .font(.largeTitle)
+            Button(action: { increaseNumber() } ) {
+                Text("Increment")
+            }
+            .padding(10)
+            Button(action: { decreaseNumber() } ) {
+                Text("Decrease")
+            }
+            .padding(10)
+            Button(action: { randomNumber() } ) {
+                Text("Random (-10 -> 10): \(randomNumberResult)")
+            }
+            .padding(10)
         }
         .padding()
+    }
+
+    func increaseNumber() {
+        count += 1
+    }
+
+    func decreaseNumber() {
+        count -= 1
+    }
+
+    func randomNumber() {
+        let number = Int(arc4random_uniform(21))
+        randomNumberResult = number - 10
+        count += randomNumberResult
     }
 }
 
